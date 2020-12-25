c863b99efe2e6abe9320bbca4b08d481e1170d1b
Ernestas-wq <ernestas.lakstinis1@gmail.com>
2020-12-25 19:21:44 +0200

    Fixed download, split some displays into a separate file

---
+++ helpers/displays.php
@@ -0,0 +1,81 @@
+<?php
+# Back button
+function displayBackButton($path)
+{
+    echo '
+<li> <form action="index.php" method="POST">
+<input type="hidden" name="file" value="' . $path . '">
+<button class="back" type="submit">
+<img class="back_img" src="assets/img/box-arrow-left.svg" alt="">
+Go back </button>
+</form></li>
+';
+}
+# User UI [Logout, etc..]
+function displayUserUI($username)
+{
+    echo '<div class="userUI">
+                    <h4 class="userUI__name">Welcome, ' . $username . '!</h4>
+                    <a class="userUI__link" href="login.php?action=logout">Logout</a>
+                    </div>';
+}
+
+# Upload modal
+
+function displayUploadModal($path)
+{
+    echo ' <div class="modal_overlay modal_overlay--active" id="uploadModal">
+
+    <form action="upload.php" method="POST" id="uploadForm" class="modal"
+    enctype="multipart/form-data">
+    <button id="closeUploadModal">Close</button>
+
+    <h3>Select file to upload: <br><br>
+     <span>(file will be saved at \uploads...)</span></h3>
+    <input type="hidden" name="file" value="' . $path . '">
+    <input class="custom-file-input" type="file" name="fileToUpload" id="fileToUpload">
+    <button class="upload" type="submit" value="upload" name="confirmUpload">
+    Submit upload</button>
+    </form>
+    ';
+}
+# New directory
+function displayAddNewDir($path)
+{
+    echo '<li>
+                <form action="" method="POST" id="new_dir_form">
+                <input type="hidden" name="file" value="' . $path . '">
+               <div class="input-container new_dir">
+                    <input type="text" id="new_dir" name="new_dir" autocomplete="off" required>
+                    <label for="new_dir" class="label-name">
+                        <span class="content-name">Add new directory
+                        <span class="new_dir__prefix"> (Press enter to confirm)</span>
+                        </span>
+                    </label>
+                </div>
+                </form>
+                </li>';
+}
+# Search display
+function displaySearch()
+{
+    echo '<li>
+    <div class="input-container search">
+    <input type="text" id="search" required autocomplete="off">
+    <label for="search" class="label-name">
+    <span class="content-name">Search files and directories</span>
+    </label>
+    </div></li>';
+};
+function displayOpenUpload($path)
+{
+    echo '<li>
+                <form action="" method="POST">
+                <input type="hidden" name="file" value="' . $path . '">
+                <input type="hidden" name="uploadModal" value="openUploadModal">
+                <button class="back" type="submit">Upload file
+                <img class="back_img" src="assets/img/cloud-arrow-up.svg" alt="">
+                </button>
+                </form>
+                </li> ';
+};
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+require('helpers/displays.php');
 session_start();
 # Defining main globals for path managing
 $root_dir = getcwd();
@@ -46,48 +47,15 @@ $base_dir = implode($sep, $tmp);
                     # Confirm delete
                     if ($_POST['confirmDelete']) {
                         $path_to_item = $_POST['confirmDelete'];
-                        print_r($path_to_item);
                         unlink($path_to_item);
                     }
-
                     echo "<ul>";
+                    # Upload
+                    displayOpenUpload($rel);
 
-
-
-                    echo '<li>
-                <form action="" method="POST">
-                <input type="hidden" name="file" value="' . $rel . '">
-                <input type="hidden" name="uploadModal" value="openUploadModal">
-                <button class="back" type="submit">Upload file
-                <img class="back_img" src="assets/img/cloud-arrow-up.svg" alt="">
-                </button>
-                </form>
-                </li> ';
-
-
-
-                    # New directory
-                    echo '<li>
-                <form action="" method="POST" id="new_dir_form">
-                <input type="hidden" name="file" value="' . $rel . '">
-               <div class="input-container new_dir">
-                    <input type="text" id="new_dir" name="new_dir" autocomplete="off" required>
-                    <label for="new_dir" class="label-name">
-                        <span class="content-name">Add new directory
-                        <span class="new_dir__prefix"> (Press enter to confirm)</span>
-                        </span>
-                    </label>
-                </div>
-                </form>
-                </li>
-                <li>
-                <div class="input-container search">
-                <input type="text" id="search" required autocomplete="off">
-                <label for="search" class="label-name">
-                <span class="content-name">Search files and directories</span>
-                </label>
-                </div></li>
-                ';
+                    # New directory & Search
+                    displayAddNewDir($rel);
+                    displaySearch();
 
                     if ($_POST['new_dir']) {
                         $new_dir = $_POST['new_dir'];
@@ -95,7 +63,7 @@ $base_dir = implode($sep, $tmp);
                     }
                     if ($rel) {
                         # Generating a back button with a back path if atleast 1 level deep
-                        # Back path is basically the path - last elementy after /
+                        # Back path is basically the path - last element after /
                         $back_path = explode($sep, $rel);
                         $back_path = array_filter($back_path);
                         array_pop($back_path);
@@ -104,14 +72,7 @@ $base_dir = implode($sep, $tmp);
                             $back_path = $sep . $back_path;
                         }
                         echo "<br>";
-                        echo '
-                    <li> <form action="index.php" method="POST">
-            <input type="hidden" name="file" value="' . $back_path . '">
-            <button class="back" type="submit">
-            <img class="back_img" src="assets/img/box-arrow-left.svg" alt="">
-            Go back </button>
-            </form></li>
-            ';
+                        displayBackButton($back_path);
                     }
                     // Getting content from new directory
                     // Printing all directories
@@ -122,15 +83,13 @@ $base_dir = implode($sep, $tmp);
                         if (is_dir($curr_dir)) {
                             #If it's a directory making a form with new path..
                             echo '
-                        <li data-search="' . $content[$i] . '"><form action="index.php"  method="POST" >
+                        <li data-search="' . $content[$i] . '">
+                        <form action="index.php"  method="POST" >
             <input type="hidden" name="file" value="' .
-                                $rel . $sep . $content[$i] .
-                                '"/>
+                                $rel . $sep . $content[$i] . '"/>
             <img class="browser_img" alt="folder" src="assets/img/folder.png">
             <button type="submit" class="folder">
-            ' . $content[$i] . ' </button>
-            </form></li>
-            ';
+            ' . $content[$i] . ' </button></form></li>';
                         }
                     }
                     #printing all files
@@ -151,8 +110,8 @@ $base_dir = implode($sep, $tmp);
                             <a target="_blank" rel="noopener noreferrer" class="openFile"
                              href="/' . $path  . ' "> ' . $content[$i] . ' </a>
                             <form action="" method="POST">
-                            <a class="download" href="download.php?file=
-                            ' . $base_dir . $sep . $content[$i] . '">
+                            <a class="download"
+                            href="download.php?file=' . $base_dir . $sep . $content[$i] . '">
                             <img class="download__img"
                             src="assets/img/cloud-arrow-down.svg" alt="trash">Download </a>
                             <input type="hidden" name="file" value="' . $rel . '">
@@ -187,28 +146,11 @@ $base_dir = implode($sep, $tmp);
                     }
                     #Upload
                     if ($_POST['uploadModal']) {
-                        echo ' <div class="modal_overlay modal_overlay--active" id="uploadModal">
-
-                    <form action="upload.php" method="POST" id="uploadForm" class="modal"
-                    enctype="multipart/form-data">
-                    <button id="closeUploadModal">Close</button>
-
-                    <h3>Select file to upload: <br><br>
-                     <span>(file will be saved at \uploads...)</span></h3>
-                    <input type="hidden" name="file" value="' . $rel . '">
-                    <input class="custom-file-input" type="file" name="fileToUpload" id="fileToUpload">
-                    <button class="upload" type="submit" value="upload" name="confirmUpload">
-                    Submit upload</button>
-                    </form>
-                    ';
+                        displayUploadModal($rel);
                     }
                     echo "</ul>";
                     # User UI corner
-
-                    echo '<div class="userUI">
-                    <h4 class="userUI__name">Welcome, ' . $_SESSION['username'] . '!</h4>
-                    <a class="userUI__link" href="login.php?action=logout">Logout</a>
-                    </div>';
+                    displayUserUI($_SESSION['username']);
                 } else {
                     echo '<h3 class="utility__message utility__message--red"> Sorry, username
                      or password was not correct.</h3>
